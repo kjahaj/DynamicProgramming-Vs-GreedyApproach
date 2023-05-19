ee5e638cf325f4703cb6d4b38a86dfa7b312155c
Klei Jahaj <119012791+kjahaj@users.noreply.github.com>
2023-05-19 21:52:54 +0200

    FullStatisticalAnalysis.py

--- Shortest Path Problem/FullStatisticalAnalysis.py
+++ Shortest Path Problem/FullStatisticalAnalysis.py
@@ -2,20 +2,16 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def MCP_DP(G):
     L = len(G)
     C = len(G[0])
     M = [[0] * C for _ in range(L)]
     A = [[0] * C for _ in range(L)]
-    
     M[0][0] = G[0][0]
-    
     for i in range(L):
         for j in range(C):
             if i == 0 and j == 0:
                 continue
-            
             m_e = minCost(M, i, j-1, L, C)
             m_ne = minCost(M, i-1, j-1, L, C)
             m_n = minCost(M, i-1, j, L, C)
@@ -29,7 +25,7 @@ def MCP_DP(G):
             
             M[i][j] = min(m_e, m_ne, m_n) + G[i][j]
     
-    return M[i][j]
+    return M[L-1][C-1]
 
 def minCost(M, i, j, L, C):
     infinity = sys.maxsize
@@ -45,20 +41,20 @@ def inGrid(i, j, L, C):
 def MCP_G(G):
     L = len(G)
     C = len(G[0])
-    sum = 0
+    total_sum = G[0][0]
     i = 0
     j = 0
-    
-    while i != L-1 or j != C-1:
-        if i == L-1:  # Reached the bottom edge, can only move horizontally
+
+    while not (i == L - 1 and j == C - 1):
+        if i == L - 1:  # Reached the bottom edge, can only move horizontally
             j += 1
-        elif j == C-1:  # Reached the right edge, can only move vertically
+        elif j == C - 1:  # Reached the right edge, can only move vertically
             i += 1
         else:
-            cost_e = G[i][j+1]  # Cost of moving east
-            cost_ne = G[i+1][j+1]  # Cost of moving northeast
-            cost_n = G[i+1][j]  # Cost of moving north
-            
+            cost_e = G[i][j + 1]  # Cost of moving east
+            cost_ne = G[i + 1][j + 1]  # Cost of moving northeast
+            cost_n = G[i + 1][j]  # Cost of moving north
+
             # Choose the minimum cost movement
             if cost_e <= cost_ne and cost_e <= cost_n:
                 j += 1  # Move east
@@ -67,20 +63,23 @@ def MCP_G(G):
                 j += 1
             else:
                 i += 1  # Move north
-        sum += G[i][j]
-    return sum
+
+        total_sum += G[i][j]
+
+    return total_sum
+
 
 
 # Parameters
-num_matrices = 1000  # Number of random matrices
+num_matrices = 3000  # Number of random matrices
 matrix_size = (8, 8)  # Size of each matrix
 
 # Create the list
 instances = []
 
 # Fill the list with random matrices
-for _ in range(10000):
-    matrix = np.random.randint(low=1, high=100, size=matrix_size)
+for _ in range(num_matrices):
+    matrix = np.random.randint(low=1, high=30, size=matrix_size)
     instances.append(matrix)
 
 relative_distances = []
@@ -89,12 +88,11 @@ for z in range(num_matrices):
     r = []
     m = MCP_DP(instances[z])
     g = MCP_G(instances[z])
-  
-      if m == 0:
-          r = 0
-      else:
-          r = (m - g) / m
-      relative_distances.append(r)
+    if m == 0:
+        r = 0
+    else:
+        r = (g-m) / g
+    relative_distances.append(r)
 
 # Compute statistical values
 mean = np.mean(relative_distances)
@@ -108,7 +106,6 @@ print(f'Standard Deviation: {std}')
 print(f'Median: {median}')
 print(f'Maximum Relative Distance: {max_rd}')
 print(f'Mean of Outliers: {outliers}')
-
 
 plt.hist(relative_distances, bins=60)
 plt.title('Histogram of Relative Distances')
